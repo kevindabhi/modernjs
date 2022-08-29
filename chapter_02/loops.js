5885818dffa5220f8d898ced9a38e66496ad1353
Kevin <kevindabhi@gmail.com>
2022-08-29 17:15:11 +0200

    updates

---
+++ chapter_02/loops.js
@@ -0,0 +1,68 @@
+// for (i = 0; i<5; i++)
+// { 
+//     console.log(i)}
+// for( initial value; condition; increment) {statement};
+
+// While loop.
+// let i = 0
+// while (i<10) {
+//     i=i+1;
+// console.log ("while loop" + " " + (i))
+// }
+
+
+//MDN Definition 
+// while (condition)
+//   statement
+
+
+// let names = ["Kevin", "Lana", "Queena", ]
+
+// for (i=0; i<names.length; i++)
+// {
+//     console.log(names[i])
+// }
+
+//Do while loop
+
+
+// let i =3;
+// do {
+//     console.log (i);
+//     i++;
+// }
+// while (i<5);
+
+//IF statement
+// if (condition) {
+//     statement
+// }
+// else if (condition 2)
+// {statement}
+
+//switch
+let grade = "C"
+
+switch(grade) {
+    case "A" : 
+    console.log("your grade is A");
+    break;
+    case "B" : 
+    console.log("your grade is B");
+    break;
+    case "C" : 
+    console.log("your grade is C");
+    break;
+    case "D" : 
+    console.log("your grade is D");
+    break;
+    case "E" : 
+    console.log("your grade is E");
+    break;
+    case "F" : 
+    console.log("your grade is F");
+    break;
+    default: "this is not a valid grade";
+    break;
+    
+}
